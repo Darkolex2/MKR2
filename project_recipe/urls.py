9f7f2ff0d5ac8047e149b80aba1b53227919a672
aleksey <alekseypetrikin3@gmail.com>
2024-05-31 16:05:41 +0300

    Initial commit with existing project structure

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path
+# project_recipe/urls.py
 
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipe.urls')),
 ]
--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+# recipe/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe
+
+class RecipeViewsTestCase(TestCase):
+
+    def setUp(self):
+        self.recipe1 = Recipe.objects.create(title="Recipe 1", description="Description 1", date="2023-01-01")
+        self.recipe2 = Recipe.objects.create(title="Recipe 2", description="Description 2", date="2023-05-15")
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipe/main.html')
+        self.assertContains(response, self.recipe1.title)
+        self.assertContains(response, self.recipe2.title)
+
+    def test_recipe_detail_view(self):
+        response = self.client.get(reverse('recipe_detail', args=[self.recipe1.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipe/recipe_detail.html')
+        self.assertContains(response, self.recipe1.title)
+        self.assertContains(response, self.recipe1.description)
